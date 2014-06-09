95b245fae95430774d89905d8acbc006fd1924db
nseewald1 <nseewald1@gmail.com>
2014-06-08 23:24:34 -0400

    Initial Upload

---
+++ server.R
@@ -0,0 +1,189 @@
+library(shiny)
+library(pwr)
+
+`%then%` <- shiny:::`%OR%`
+
+shinyServer(function(input,output,session){
+   
+  ##### DESIGN B IMAGE HEADER #####
+  
+  output$designBimg <- renderImage({
+       filename<-filename<-normalizePath(file.path('./www',paste('SMARTdesignB_',input$firstDTRcompareB,'_',input$secondDTRcompareB,'.gif',sep='')))
+       list(src=filename)
+   },deleteFile=FALSE)
+  
+  ##### DESIGN B PROBABILITY INPUT #####
+
+  substringDTR1B <- reactive({
+    DTR1 <-paste(input$firstDTRcompareB)
+    firstStage1<-substr(DTR1,1,1)
+    secondStageNR1<-substr(DTR1,6,6)
+    
+    if (input$selectOutcomeB==1 & input$selectBinarySuccessB <= 2){
+      validate(
+        need(DTR1!=0, "Please select a first DTR.")
+      )
+    }    
+    
+    return(c(DTR1, firstStage1,secondStageNR1))
+    
+  })
+  
+  substringDTR2B <- reactive({
+    DTR2 <-paste(input$secondDTRcompareB)
+    firstStage2<-substr(DTR2,1,1)
+    secondStageNR2<-substr(DTR2,6,6)
+    
+    if (input$selectOutcomeB==1 && input$selectBinarySuccessB <= 2){
+      validate(
+        need(DTR2!=0, "Please select a second DTR.")
+      )
+    }    
+    
+    return(c(DTR2, firstStage2, secondStageNR2))
+    
+  })
+  
+  output$binaryDTRinputsB <- renderUI({
+
+    if(input$selectBinarySuccessB==1 && substringDTR1B()[1] != substringDTR2B()[1]){
+      controlInputs<-c(numericInput("DTRsuccB1",label="Probability of Success for First DTR",value=0,min=0,max=1,step=0.0001),
+                       numericInput("DTRsuccB2",label="Probability of Success for Second DTR",value=0,min=0,max=1,step=0.0001)
+      )
+    }
+    
+    if(input$selectBinarySuccessB==2 && substringDTR1B()[1] != substringDTR2B()[1]){
+      if(substringDTR1B()[2]==substringDTR2B()[2]){
+        controlInputs<-c(numericInput("marginalSecondStageNRB1",label=paste("P(S|",substringDTR1B()[2],",nr,",substringDTR1B()[3],")",sep=""),
+                                      value=0,min=0,max=1,step=0.0001),
+                         numericInput("marginalSecondStageNRB2",label=paste("P(S|",substringDTR2B()[2],",nr,",substringDTR2B()[3],")",sep=""),
+                                      value=0,min=0,max=1,step=0.0001)
+                         )
+      }
+      else{
+        controlInputs<-c(numericInput("marginalFirstStageB1",label=paste("P(S|",substringDTR1B()[2],",r)",sep=""),value=0,min=0,max=1,step=0.0001),
+                         numericInput("marginalSecondStageNRB1",label=paste("P(S|",substringDTR1B()[2],",nr,",substringDTR1B()[3],")",sep=""),
+                                      value=0,min=0,max=1,step=0.0001),
+                         numericInput("marginalFirstStageB2",label=paste("P(S|",substringDTR2B()[2],",r)",sep=""),value=0,min=0,max=1,step=0.0001),
+                         numericInput("marginalSecondStageNRB2",label=paste("P(S|",substringDTR2B()[2],",nr,",substringDTR2B()[3],")",sep=""),
+                                      value=0,min=0,max=1,step=0.0001)
+        )
+      }
+    }
+    
+    if(input$selectBinarySuccessB==1 && substringDTR1B()[2]==substringDTR2B()[2]){
+      updateSelectInput(session,inputId="selectBinarySuccessB",selected=2)      
+    }
+    
+    if(input$selectBinarySuccessB==3){
+      controlInputs<-c(numericInput("targetDiffB",label="Target Difference in Success Probabilities",value=0.1,min=0.0001,max=0.5,step=0.0001))
+    }
+    
+    if(input$selectBinarySuccessB==4){
+      controlInputs<-c(numericInput("targetORB",label="Target Odds Ratio of Success",value=2,min=0,step=0.0001))
+    }
+    
+    if(substringDTR1B()[1]==substringDTR2B()[1] && input$selectBinarySuccessB <= 2){
+      controlInputs<-(helpText("Select two non-identical DTRs to continue."))
+    }
+    
+    controlInputs
+  })
+
+  ##### DESIGN B RESULT #####
+  
+  dataCompilerB <- reactive({
+    
+    validate(need(!(is.na(input$respB)), "Please provide a response probability. If unknown, enter 0 for a conservative estimate of power or sample size."))
+    
+    if(input$selectOutcomeB==1 && input$selectBinarySuccessB==1){
+      validate(
+        need(input$firstDTRcompareB != 0, "Select a first DTR above."),
+        need(input$secondDTRcompareB != 0, "Select a second DTR above.") %then%
+        need(input$DTRsuccB1 != input$DTRsuccB2, "Please provide unique success probabilities for each DTR. Sample size is indeterminate for equal DTR probabilities.")  
+      )
+      return(c(input$DTRsuccB1,input$DTRsuccB2))
+    }
+    
+    if(input$selectOutcomeB==1 && input$selectBinarySuccessB==2){
+          
+      if(substringDTR1B()[2]==substringDTR2B()[2]){
+        pDTR1 <- input$marginalSecondStageNRB1
+        pDTR2 <- input$marginalSecondStageNRB2
+      }
+      
+      else{
+        pDTR1<- input$respB * input$marginalFirstStageB1 + (1-input$respB)*input$marginalSecondStageNRB1
+        pDTR2<- input$respB * input$marginalFirstStageB2 + (1-input$respB)*input$marginalSecondStageNRB2
+      }
+      
+      validate(
+        need(input$firstDTRcompareB != 0, "Select a first DTR above."),
+        need(input$firstDTRcompareB != 0, "select a second DTR above."),
+        need(pDTR1 != pDTR2, "The provided marginal probabilities yield identical overall DTR success probabilities. Sample size is indeterminate for equal DTR probabilities. Please adjust your inputs.")
+      )
+      
+      return(c(pDTR1,pDTR2))
+     }
+    
+    if(input$selectOutcomeB==1 && input$selectBinarySuccessB==3){
+      validate(
+        need(input$targetDiffB <= 0.5, "Target difference must be less than 0.5 to be valid input"),
+        need(input$targetDiffB > 0, "Target difference must be greater than 0. Sample size is indeterminate for equal DTR probabilities.
+             Please adjust your inputs.")
+      )
+      return(c(0.5,0.5+input$targetDiffB))
+    }
+    
+    if(input$selectOutcomeB==1 && input$selectBinarySuccessB==4){
+      validate(
+        need(input$targetORB != 1, "Sample size is indeterminate for an odds ratio of 1. Please enter a different target odds ratio."),
+        need(input$targetORB != 0, "Sample size is indeterminate for an odds ratio of 0. Please enter a different target odds ratio.")
+      )
+      return(c(0.5,input$targetORB/(1+input$targetORB)))
+    }
+    
+    if(input$selectOutcomeB==2){
+      validate(
+        need(input$effectSizeB != 0, "Sample size is indeterminate for an effect size of 0. Please enter a different target effect size.")
+      )
+      return(input$effectSizeB)
+    }
+    
+    #if(input$)
+    
+  })
+
+  selectEffectB <- reactive({
+    if(substringDTR1B()[2] != substringDTR2B()[2]){
+      designEffect <- 2*(2*(1-input$respB)+input$respB)
+    }
+    else{
+      designEffect <- 4/(1-input$respB)
+    }
+    return(designEffect)
+  })
+
+  output$binarySampleSizeB <- renderPrint({
+    designEffect<-selectEffectB()
+    rawSampleSize<-power.prop.test(p1=dataCompilerB()[1],p2=dataCompilerB()[2],power=input$inputPowerB,sig.level=input$alphaB)$n
+    finalSampleSize<-ceiling(designEffect * rawSampleSize)
+    cat("N=",finalSampleSize)
+  })
+
+  output$binaryPowerB <- renderPrint({
+    designEffect<-selectEffectB()
+    size<-input$inputSampleSizeB/designEffect
+    finalPower<-round(power.prop.test(p1=dataCompilerB()[1],p2=dataCompilerB()[2],n=size,sig.level=input$alphaB)$power,digits=3)
+    cat("Power =",finalPower)
+  })
+  
+  output$continuousSampleSizeB <- renderPrint({
+    designEffect<selectEffectB()
+    rawSampleSize<-pwr.norm.test(d=dataCompilerB(),sig.level=input$alphaB,power=input$inputPowerB,alternative="two.sided")
+    finalSampleSize<-ceiling(2*designEffect*rawSampleSize$n)
+    cat("N=",finalSampleSize)
+  })
+  
+
+})
---
+++ ui.R
@@ -0,0 +1,219 @@
+library(shiny)
+
+shinyUI(
+  navbarPage("SMART Sample Size Calculator",
+
+             ##### HOME TAB#####
+             
+             tabPanel("Home",
+                      sidebarPanel(h3("Sidebar information"),
+                                   p("Welcome to the app. We'll put some instructions here, etc. etc."),
+                                   p("Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                      Integer nec odio. Praesent libero. Sed cursus ante dapibus 
+                                      diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. 
+                                      Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed 
+                                      augue semper porta. Mauris massa. Vestibulum lacinia arcu 
+                                      eget nulla. Class aptent taciti sociosqu ad litora torquent 
+                                      per conubia nostra, per inceptos himenaeos. Curabitur 
+                                      sodales ligula in libero.")),
+                      mainPanel(h1("Introduction"),br(),
+                                p("Choose a design by clicking the cooresponding tab at the top of the window."), br(),
+                                fluidRow(column(6,img(src="SMARTdesignA.gif")), column(6,img(src="SMARTdesignB_0_0.gif"))),
+                                fluidRow(column(6,helpText("Design A")),column(6,helpText("Design B"))),
+                                fluidRow(column(6,img(src="SMARTdesignC.gif")), column(6,img(src="SMARTdesignD.gif"))),
+                                fluidRow(column(6,helpText("Design C")),column(6,helpText("Design D"))),
+                                h2("More Information"),
+                                p("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed 
+                                  cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. 
+                                  Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. "),
+                                p("Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur 
+                                  sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam.
+                                  In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc 
+                                  egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis 
+                                  quis ligula lacinia aliquet. Mauris ipsum. "),
+                                p("Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit.
+                                  Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. 
+                                  Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. 
+                                  Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum
+                                  sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit.")
+                      )
+             ), 
+             tabPanel("Design A",
+                      sidebarPanel(h3("About this design:"),
+                                   p("Include information about the design, its requirements, and describe the input.")
+                      ),
+                      mainPanel(h1("Design A"),
+                                img(src="SMARTdesignA.gif"),
+                                fluidRow(
+                                  column(6,
+                                         radioButtons("selectOutcomeA", label="Select a Type of Outcome:",
+                                                      choices=list("Binary"=1,"Continuous"=2),selected=1)
+                                  ),
+                                  column(6,
+                                         radioButtons("selectSuccessA",label="Select a method of providing Success Probabilities:",
+                                                      choices=list("Overall DTR Success"=1, "Marginal Treatment Success"=2, "Target Difference"=3),
+                                                      selected=1
+                                         )
+                                  )
+                                ),
+                                fluidRow(
+                                  column(6,
+                                         numericInput("respA.1",label="Probability of Response to First-Stage Treatment:",value=0,min=0,max=1,step=0.0001)
+                                  ),
+                                  column(6,
+                                         conditionalPanel(condition="input.selectSuccessA==1",
+                                                          numericInput("DTRsucc1A",label="Probability of Success for DTR ArCAnrE",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("DTRsucc2A",label="Probability of Success for DTR ArCAnrF",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("DTRsucc3A",label="Probability of Success for DTR ArDAnrE",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("DTRsucc4A",label="Probability of Success for DTR ArDAnrF",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("DTRsucc5A",label="Probability of Success for DTR BrHBnrJ",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("DTRsucc6A",label="Probability of Success for DTR BrHBnrK",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("DTRsucc7A",label="Probability of Success for DTR BrIBnrJ",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("DTRsucc8A",label="Probability of Success for DTR BrIBnrK",value=0,min=0,max=1,step=0.0001)
+                                         ),
+                                         conditionalPanel(condition="input.selectSuccessA==2",
+                                                          numericInput("marSucc1A",label="P(S | A, r, C)",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("marSucc2A",label="P(S | A, r, D)",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("marSucc3A",label="P(S | A, nr, E)",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("marSucc4A",label="P(S | A, nr, F)",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("marSucc5A",label="P(S | B, r, H)",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("marSucc6A",label="P(S | B, r, I)",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("marSucc7A",label="P(S | B, nr, J)",value=0,min=0,max=1,step=0.0001),
+                                                          numericInput("marSucc8A",label="P(S | B, nr, K)",value=0,min=0,max=1,step=0.0001)
+                                         ),
+                                         conditionalPanel(condition="input.selectSuccessA==3",
+                                                          numericInput("effectSize",label="Target Difference in Effects:", value=0.5,min=0,max=1,step=0.00001)
+                                         )
+                                  )
+                                ),
+                                numericInput("sampleSize",label="Sample Size",value=0) 
+                      )
+                      ),
+             
+             ########### DESIGN B ##########
+             
+             tabPanel("Design B",
+                      
+                      ##### B SIDEBAR #####
+                      
+                      sidebarPanel(h3("About this design:"),
+                                   p("Include information about the design, its requirements, and describe the input."),
+                                   p(strong("Note:"), em("All inputs must be given in decimal form (with leading zero), and can be precise to four decimal places.
+                                     Fractional input is disallowed."))
+                        ),
+                      mainPanel(
+                        
+                        ##### B PAGE HEADER #####
+                        
+                        h1("Design B"),
+                        imageOutput("designBimg",height=450),
+                        tags$hr(),
+                        
+                                               
+                        ##### B OUTCOME OPTIONS #####
+                        
+                          h3("Outcome Options"),
+                          fluidRow(
+                                  column(6,
+                                         selectInput("selectOutcomeB", label="Select a Type of Outcome:",
+                                                      choices=list("Binary"=1,"Continuous"=2))
+                                         ),
+                                  column(6,
+                                         conditionalPanel(condition="input.selectOutcomeB==1",
+                                                          selectInput("selectBinarySuccessB",label="Select a method of providing Success Probabilities:",
+                                                                       choices=list("Overall DTR Success"=1, "Marginal Treatment Success"=2, 
+                                                                                    "Target Difference"=3, "Target Odds Ratio"=4))
+                                         )
+                                  )
+                          ),
+                          tags$hr(),
+                        
+                        ##### B DTR SELECTION #####
+                        
+                        h3("DTR Selection"),
+                        fluidRow(
+                          column(6,
+                                 selectInput("firstDTRcompareB",label="Compare DTR",
+                                             choices=list("[Choose First DTR]"=0, "ArAnrC"="ArAnrC", "ArAnrD"="ArAnrD", "BrBnrE"="BrBnrE", "BrBnrF"="BrBnrF"),
+                                             selected=0)
+                          ),
+                          column(6,
+                                 selectInput("secondDTRcompareB",label="to DTR",
+                                             choices=list("[Choose Second DTR]"=0, "ArAnrC"="ArAnrC", "ArAnrD"="ArAnrD", "BrBnrE"="BrBnrE", "BrBnrF"="BrBnrF"),
+                                             selected=0)  
+                          )
+                        ),
+                        fluidRow(
+                          conditionalPanel(condition="input.firstDTRcompareB==input.secondDTRcompareB & input.firstDTRcompareB != 0",
+                                           p("ERROR: Please select two different DTRs to compare.",style="color:red")),
+                          conditionalPanel(condition="input.firstDTRcompareB.substr(1,1)==input.secondDTRcompareB.substr(1,1) && input.firstDTRcompareB != 0 && input.selectBinarySuccessB<=2",
+                                           helpText("NOTE: For DTRs with the same initial treatment, Overall DTR Success is not a valid input method."))
+                        ),
+                        tags$hr(),
+                        
+                          ##### B PROBABILITY INPUT #####
+                        
+                          h3("Probability Inputs"),
+                          fluidRow(
+                                   numericInput("respB",label="Probability of Response to First-Stage Treatment:",value=0,min=0,max=1,step=0.0001),
+                                   conditionalPanel(condition="input.selectOutcomeB==2",
+                                                    numericInput("effectSizeB",label="Standardized Effect Size:",value=0,min=0,step=0.0001)
+                                   ),
+                                   conditionalPanel(condition="input.selectOutcomeB==1",
+                                                    uiOutput("binaryDTRinputsB")
+                                   )
+                          ),
+                          tags$hr(),
+                        
+                        ##### B ERROR SPECIFICATION #####
+                        
+                          h3("Error Specification"),
+                          fluidRow(
+                                  column(6,
+                                         radioButtons("selectResultsB",label="Select the desired result.",
+                                                      choices=list("Sample Size" = "sample", "Power" = "power"),
+                                                      selected="sample")
+                                         ),
+                                  column(6,
+                                         numericInput("alphaB",label="Type I Error (Alpha):",value=0.05,min=0,max=1,step=0.0001),
+                                         conditionalPanel(condition="input.selectResultsB=='sample'",
+                                                          numericInput("inputPowerB",label="Power of Trial:",value=0.8, min=0, max=1,step=0.0001)
+                                          ),
+                                         conditionalPanel(condition="input.selectResultsB=='power'",
+                                                          numericInput("inputSampleSizeB",label="Total Sample Size of Trial:",value=0, min=0)
+                                  )
+                                )
+                                ),
+                          tags$hr(),
+
+                        ##### B RESULTS #####
+                
+                          h3("Results"),
+                          conditionalPanel(condition="input.selectOutcomeB==1 & input.selectResultsB=='sample'",
+                                           h4(textOutput("binarySampleSizeB"), style = "color:blue")
+                                           ),
+                          conditionalPanel(condition="input.selectOutcomeB==1 & input.selectResultsB=='power'",
+                                           h4(textOutput("binaryPowerB"),style="color:blue")
+                                           ),
+                          conditionalPanel(condition="input.selectOutcomeB==2 & input.selectResultsB=='sample'",
+                                           h4(textOutput("continuousSampleSizeB"),style="color:blue"))
+                    )
+             ),
+             tabPanel("Design C",
+                      sidebarPanel(h3("About this design:"),
+                                   p("Include information about the design, its requirements, and describe the input.")
+                      ),
+                      mainPanel(h1("Design C"),
+                                img(src="SMARTdesignC.gif")
+                      )
+             ),
+             tabPanel("Design D",
+                      sidebarPanel(h3("About this design:"),
+                                   p("Include information about the design, its requirements, and describe the input.")
+                      ),
+                      mainPanel(h1("Design D"),
+                                img(src="SMARTdesignD.gif")
+                      )
+             )
+  )
+)
