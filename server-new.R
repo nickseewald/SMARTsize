dbdd22c2631d6767ec349e812a13372716f020e7
Nick Seewald <nseewald@umich.edu>
2017-09-21 19:53:36 -0400

    Clean-up

--- server-new.R
+++
@@ -1,6 +0,0 @@
-function(input, output, session) {
-  output$renderPageHome <- renderUI({
-    source(file.path("partials/home.R"),local=TRUE)
-  })
-  source(file.path("controllers/modalButton.R"))
-}
